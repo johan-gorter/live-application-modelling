818e29e3e8ea66006def88c9e4eb5ca13c0a9b10
Gast <Gast@LAP-1459.everest.nl>
2012-01-11 22:04:59 +0100

    MIA

---
+++ lberunner/app/custom/ritadministratie/BeginstandDeductionCustomization.java
@@ -0,0 +1,14 @@
+package custom.ritadministratie;
+
+import lbe.engine.DeductionContext;
+import lbe.model.deduction.Deduction;
+import app.ritadministratie.Stand;
+
+public class BeginstandDeductionCustomization extends Deduction<Stand> {
+
+	@Override
+	public Stand deduct(DeductionContext context) {
+		return null;
+	}
+
+}
